683def6f487ef97d0a064d6b9bb3407f4ca84120
IamEp1c <Qanjum96@gmail.com>
2024-09-29 17:59:52 -0400

    added some terraform information

--- Main.tf
+++ Main.tf
@@ -10,18 +10,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_instance" "waqasEc2" {
-  ami           = "ami-0ebfd941bbafe70c6"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-01da2be2222ba9181"
-  tags = {
-    Name = "ExampleEc2"
-  }
-}
-
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
-  }
-}
