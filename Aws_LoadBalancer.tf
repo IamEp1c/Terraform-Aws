16f60937ab3b9eff0a1ad5fa23585463ae5730c0
IamEp1c <Qanjum96@gmail.com>
2024-10-14 21:55:37 -0400

    running into an issue preventing access logs to be created

--- Aws_LoadBalancer.tf
+++ Aws_LoadBalancer.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "creatingLB" {
   internal           = false
   load_balancer_type = "application"
   #security_groups    = [aws_security_group.lb_sg.id]
-  subnets =           [	"subnet-0410ec127adfd11be" , "subnet-08430d9514d9cc592"]
+  subnets =           [	"subnet-0aef788bcd8d5319f" , "subnet-010f68d2392b7166e"]
   
   enable_deletion_protection = true
 
@@ -21,16 +21,53 @@ resource "aws_lb" "creatingLB" {
 resource "aws_s3_bucket" "s3_bucket_for_logs_01" {
   bucket        = "waqas-test-bucket"
   force_destroy = true
+  # depends_on = [ aws_s3_bucket_server_side_encryption_configuration.example ]
 }
 
-# resource "aws_subnet" "main" {
-#   vpc_id     = aws_default_vpc.default.id
-#   # cidr_block = "172.31.0.0/20"
+# resource "aws_kms_key" "mykey" {
+#   description             = "This key is used to encrypt bucket objects"
+#   deletion_window_in_days = 10
+# }
 
-#   tags = {
-#     Name = "Main"
+# resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+#   bucket = aws_s3_bucket.s3_bucket_for_logs_01.id
+
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       kms_master_key_id = aws_kms_key.mykey.arn
+#       sse_algorithm     = "aws:kms"
+#     }
 #   }
 # }
+
+# import {
+#   to = aws_s3_bucket_policy.TestBucketPolicyLB
+#   id = "my-tf-test-bucket"
+# }
+resource "aws_s3_bucket_policy" "TestBucketPolicyLB" {
+  bucket = aws_s3_bucket.s3_bucket_for_logs_01.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [aws_lb.creatingLB.arn]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.s3_bucket_for_logs_01.arn,
+      "${aws_s3_bucket.s3_bucket_for_logs_01.arn}/*",
+    ]
+  }
+}
+
 
 # need vpc for IG, Subnet etc. Have one default vpc here for that purpose
 resource "aws_default_vpc" "default" {
