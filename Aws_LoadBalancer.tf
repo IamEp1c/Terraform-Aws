28db0de1a1fa15f91715e568951fd1195dd00783
IamEp1c <Qanjum96@gmail.com>
2024-10-20 23:05:20 -0400

    continuing to learn about AWS through terraform

--- Aws_LoadBalancer.tf
+++ Aws_LoadBalancer.tf
@@ -1,11 +1,10 @@
-resource "aws_lb" "creatingLB" {
+resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  #security_groups    = [aws_security_group.lb_sg.id]
-  subnets =           [	"subnet-0aef788bcd8d5319f" , "subnet-010f68d2392b7166e"]
+  security_groups    = [aws_security_group.allow_tls.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
 
-  
   enable_deletion_protection = true
 
   access_logs {
@@ -15,9 +14,48 @@ resource "aws_lb" "creatingLB" {
   }
 
   tags = {
-    Environment = "Dev"
+    Environment = "dev"
   }
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
