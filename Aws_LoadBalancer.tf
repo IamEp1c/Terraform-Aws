3cf873e7fa5eb67adb95e1c3e55d0f746429f1c4
IamEp1c <Qanjum96@gmail.com>
2024-10-20 23:27:56 -0400

    facing new error IPv6 CIDR

--- Aws_LoadBalancer.tf
+++ Aws_LoadBalancer.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "test" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.allow_tls.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  subnets            = ["subnet-016f5fb94dbc0127b", "subnet-04c2cfb1bdb5da111"]
 
   enable_deletion_protection = true
 
@@ -21,7 +21,7 @@ resource "aws_lb" "test" {
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_default_vpc.default.id
 
   tags = {
     Name = "allow_tls"
@@ -30,7 +30,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = aws_default_vpc.default.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -38,7 +38,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  cidr_ipv6         = aws_default_vpc.default.ipv6_cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -57,42 +57,12 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
 }
 
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_default_vpc.default.id
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
 resource "aws_s3_bucket" "s3_bucket_for_logs_01" {
   bucket        = "waqas-test-bucket"
   force_destroy = true
   # depends_on = [ aws_s3_bucket_server_side_encryption_configuration.example ]
 }
 
-# resource "aws_kms_key" "mykey" {
-#   description             = "This key is used to encrypt bucket objects"
-#   deletion_window_in_days = 10
-# }
-
-# resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-#   bucket = aws_s3_bucket.s3_bucket_for_logs_01.id
-
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       kms_master_key_id = aws_kms_key.mykey.arn
-#       sse_algorithm     = "aws:kms"
-#     }
-#   }
-# }
-
-# import {
-#   to = aws_s3_bucket_policy.TestBucketPolicyLB
-#   id = "my-tf-test-bucket"
-# }
 resource "aws_s3_bucket_policy" "TestBucketPolicyLB" {
   bucket = aws_s3_bucket.s3_bucket_for_logs_01.id
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
