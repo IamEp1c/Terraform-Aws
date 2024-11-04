2cae59c352e26217e917c84b7cbdf5a3b21df730
IamEp1c <Qanjum96@gmail.com>
2024-11-03 19:23:47 -0500

    verified resource is being populated via console but error with environment

--- Aws_Elastic_Beanstalk.tf
+++ Aws_Elastic_Beanstalk.tf
@@ -4,10 +4,16 @@
 
 resource "aws_elastic_beanstalk_application" "ebaTest" {
   name        = "Eba-Test"
+  description = "an EBA app to host static website"
 }
 
 resource "aws_elastic_beanstalk_environment" "tfenvtest" {
   name                = "tf-test-name"
   application         = aws_elastic_beanstalk_application.ebaTest.name
-  solution_stack_name = "64bit Amazon Linux 2015.03 v2.0.3 running Go 1.4"
+  solution_stack_name = "64bit Amazon Linux 2 v3.11.2 running Go 1"
 }
+
+
+
+
+
--- Aws_LoadBalancer.tf
+++ Aws_LoadBalancer.tf
@@ -1,100 +1,100 @@
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.allow_tls.id]
-  subnets            = ["subnet-01c143784f8be9cfe", "subnet-09d40cdd758f21bd1"]
+# resource "aws_lb" "test" {
+#   name               = "test-lb-tf"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.allow_tls.id]
+#   subnets            = ["subnet-01c143784f8be9cfe", "subnet-09d40cdd758f21bd1"]
 
-  enable_deletion_protection = true
+#   enable_deletion_protection = true
 
-  access_logs {
-    bucket  = aws_s3_bucket.s3_bucket_for_logs_01.id
-    prefix  = "test-lb"
-    enabled = true
-  }
+#   access_logs {
+#     bucket  = aws_s3_bucket.s3_bucket_for_logs_01.id
+#     prefix  = "test-lb"
+#     enabled = true
+#   }
 
-  tags = {
-    Environment = "dev"
-  }
-}
+#   tags = {
+#     Environment = "dev"
+#   }
+# }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_default_vpc.default.id
+# resource "aws_security_group" "allow_tls" {
+#   name        = "allow_tls"
+#   description = "Allow TLS inbound traffic and all outbound traffic"
+#   vpc_id      = aws_default_vpc.default.id
 
-  tags = {
-    Name = "allow_tls"
-  }
-}
+#   tags = {
+#     Name = "allow_tls"
+#   }
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_default_vpc.default.cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv4         = aws_default_vpc.default.cidr_block
+#   from_port         = 443
+#   ip_protocol       = "tcp"
+#   to_port           = 443
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_default_vpc.default.ipv6_cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv6         = aws_default_vpc.default.ipv6_cidr_block
+#   from_port         = 443
+#   ip_protocol       = "tcp"
+#   to_port           = 443
+# }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1" # semantically equivalent to all ports
+# }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-
-resource "aws_s3_bucket" "s3_bucket_for_logs_01" {
-  bucket        = "waqas-test-bucket"
-  force_destroy = true
-  # depends_on = [ aws_s3_bucket_server_side_encryption_configuration.example ]
-}
-
-resource "aws_s3_bucket_policy" "TestBucketPolicyLB" {
-  bucket = aws_s3_bucket.s3_bucket_for_logs_01.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-}
-
-data "aws_iam_policy_document" "allow_access_from_another_account" {
-  statement {
-    principals {
-      type        = "AWS"
-      // added elb root id still throwing error 400
-      identifiers = ["arn:aws:iam::127311923021:root"]
-    }
-
-    actions = [
-      "s3:GetObject",
-      "s3:ListBucket",
-    ]
-
-    resources = [
-     aws_s3_bucket.s3_bucket_for_logs_01.arn,
-     "${aws_s3_bucket.s3_bucket_for_logs_01.arn}/*",
-    ]
-  }
-}
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv6         = "::/0"
+#   ip_protocol       = "-1" # semantically equivalent to all ports
+# }
 
 
-# need vpc for IG, Subnet etc. Have one default vpc here for that purpose
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
-  }
-}
+# resource "aws_s3_bucket" "s3_bucket_for_logs_01" {
+#   bucket        = "waqas-test-bucket"
+#   force_destroy = true
+#   # depends_on = [ aws_s3_bucket_server_side_encryption_configuration.example ]
+# }
 
-# made a new LB, facing an error when it came to cidr_ipv6 = aws_default_vpc.default.ipv6_cidr_block. I was able to add manually through the UI
-# will attempt again through terraform
+# resource "aws_s3_bucket_policy" "TestBucketPolicyLB" {
+#   bucket = aws_s3_bucket.s3_bucket_for_logs_01.id
+#   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+# }
+
+# data "aws_iam_policy_document" "allow_access_from_another_account" {
+#   statement {
+#     principals {
+#       type        = "AWS"
+#       // added elb root id still throwing error 400
+#       identifiers = ["arn:aws:iam::127311923021:root"]
+#     }
+
+#     actions = [
+#       "s3:GetObject",
+#       "s3:ListBucket",
+#     ]
+
+#     resources = [
+#      aws_s3_bucket.s3_bucket_for_logs_01.arn,
+#      "${aws_s3_bucket.s3_bucket_for_logs_01.arn}/*",
+#     ]
+#   }
+# }
+
+
+# # need vpc for IG, Subnet etc. Have one default vpc here for that purpose
+# resource "aws_default_vpc" "default" {
+#   tags = {
+#     Name = "Default VPC"
+#   }
+# }
+
+# # made a new LB, facing an error when it came to cidr_ipv6 = aws_default_vpc.default.ipv6_cidr_block. I was able to add manually through the UI
+# # will attempt again through terraform
