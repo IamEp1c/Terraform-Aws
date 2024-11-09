940021c12107d904030531fa01100c7434e70f56
IamEp1c <Qanjum96@gmail.com>
2024-11-09 13:04:06 -0500

    added dynamoDB will update it to to create tables and items

--- Aws_Elastic_Beanstalk.tf
+++ Aws_Elastic_Beanstalk.tf
@@ -2,16 +2,23 @@
 ## Elastic Beanstalk allows you to deploy and manage applications in the AWS cloud without worrying about the infrastructure 
 ## that runs those applications.
 
-resource "aws_elastic_beanstalk_application" "ebaTest" {
-  name        = "Eba-Test"
-  description = "an EBA app to host static website"
-}
+# creates an s3 bucket make sure you delete
 
-resource "aws_elastic_beanstalk_environment" "tfenvtest" {
-  name                = "tf-test-name"
-  application         = aws_elastic_beanstalk_application.ebaTest.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.11.2 running Go 1"
-}
+# resource "aws_elastic_beanstalk_application" "ebaTest1" {
+#   name        = "Eba-Test"
+#   description = "an EBA app to host static website, dev environment"
+# }
+
+# running issue with this resource 11/03/2024 , wasn't able to add
+
+# resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+#   name                = "tf-test-name"
+#   application         = aws_elastic_beanstalk_application.ebaTest.name
+#   solution_stack_name = "64bit Amazon Linux 2 v3.11.2 running Go 1"
+# }
+
+
+
 
 
 
