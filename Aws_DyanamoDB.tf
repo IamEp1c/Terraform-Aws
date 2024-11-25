2f357dec97994fddb08df06b2e6b2bd8ae47afe3
IamEp1c <Qanjum96@gmail.com>
2024-11-25 16:26:16 -0500

    was able to create dynamboDB table and add an item to the table

--- Aws_DyanamoDB.tf
+++ Aws_DyanamoDB.tf
@@ -1,45 +1,47 @@
 resource "aws_dynamodb_table" "dynamoDbPoc" {
-  name             = "dynamoDBPOC"
-  hash_key         = "TestTableHashKey"
-  billing_mode     = "PAY_PER_REQUEST"
-  stream_enabled   = true
-  stream_view_type = "NEW_AND_OLD_IMAGES"
+  name           = "dynamoDB_POC"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "id"
 
   attribute {
-    name = "TestTableHashKey"
+    name = "id"
     type = "S"
   }
-
-  replica {
-    region_name = "us-east-2"
-  }
-
-#   replica {
-#     region_name = "us-west-2"
-#   }
 }
+
+# resource "aws_dynamodb_table_item" "DbTableItems" {
+#   table_name = aws_dynamodb_table.dynamoDbPoc.name
+#   hash_key = "id"  # Partition key: id
+
+#   # Add the 'id' key to the item
+#   item = jsonencode({
+#     id    = "123"                # The partition key is 'id'
+#     email = "user@example.com"
+#   })
+# }
 
 resource "aws_dynamodb_table_item" "DbTableItems" {
   table_name = aws_dynamodb_table.dynamoDbPoc.name
-  hash_key = "userId"
-item = jsonencode({
-    userId    = { S = "user123" }
+  hash_key = "id"
+  item = jsonencode({
+    id    = { S = "user123" }
     userName  = { S = "slogenShade" }
     userEmail = { S = "slogenShade123@example.com" }
   })
 }
 
-data "aws_dynamodb_table" "users" {
- name = aws_dynamodb_table_item.DbTableItems.table_name  # Specify the name of your DynamoDB table
-}
+# data "aws_dynamodb_table" "users" {
+#  name = aws_dynamodb_table_item.DbTableItems.table_name  # Specify the name of your DynamoDB table
+# }
 
-data "aws_dynamodb_table_item" "user_item" {
- table_name = data.aws_dynamodb_table.users.name
- // key = "userId"
- // added a hashkey here 
- key = aws_dynamodb_table_item.DbTableItems.hash_key
-  # key = <<key # expects a value of the key here
-}
+# data "aws_dynamodb_table_item" "user_item" {
+#  table_name = data.aws_dynamodb_table.users.name
+#  // key = "userId"
+#  // added a hashkey here 
+#  key = aws_dynamodb_table_item.DbTableItems.hash_key
+#   # key = <<key # expects a value of the key here
+# }
 
 
 
