3c1a974e06a08ab13ab758d21e0eeb114b7f588b
IamEp1c <Qanjum96@gmail.com>
2024-11-27 17:26:01 -0500

    created rest api, but was not able to see in console

--- RestAPI.tf
+++ RestAPI.tf
@@ -1,7 +1,13 @@
-resource "aws_api_gateway_rest_api" "name" {
+resource "aws_api_gateway_rest_api" "myDemoApi" {
   name = "basicRestApi"
   
   endpoint_configuration {
     types = ["REGIONAL"]
   }
+}
+
+resource "aws_api_gateway_resource" "api_gateway_resource" {
+  rest_api_id = aws_api_gateway_rest_api.myDemoApi.id
+  parent_id = aws_api_gateway_rest_api.myDemoApi.root_resource_id
+  path_part   = "mydemoresource"
 }
